14e17995b32d398b52de8da47ce4507f35b58568
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-08-02 01:18:12 +0300

    ➕ Add SubArray Extension method.

---
+++ ATB.DA/Extensions/ArrayExtenstions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATB.DA.Extensions
+{
+    public static class ArrayExtenstions
+    {
+        public static T[] SubArray<T> (this T[] array, int offset, int length)
+        {
+            if(array == null)
+                throw new ArgumentNullException("array");
+
+            return array.Skip(offset)
+                   .Take(length)
+                   .ToArray();
+        }
+    }
+}

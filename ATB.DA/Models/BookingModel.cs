d3274ce0d2227931966608c7b7580735b6013886
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-08-01 11:58:15 +0300

    ✅ Add BookingModel and BookingModelTests
    ➕ Methods to covert from CSV and object representation.

---
+++ ATB.DA/Models/BookingModel.cs
@@ -0,0 +1,40 @@
+﻿using ATB.DA.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATB.DA.Models
+{
+    public record BookingModel(ulong UserId, ulong FlightId, FlightClassEnum FlightClass) 
+    {
+        /// <summary>
+        /// Return a object of booking model from the CSV representation.
+        /// </summary>
+        /// <param name="csv"></param>
+        /// <returns>BookingModel</returns>
+        public static BookingModel FromCSV(string csv)
+        {
+            string[] bookingInfo = csv.Split(",");
+            return new BookingModel
+                (
+                    ulong.Parse(bookingInfo[0]),
+                    ulong.Parse(bookingInfo[1]),
+                    (FlightClassEnum)int.Parse(bookingInfo[2])
+                );
+        }
+        /// <summary>
+        /// Return the CSV representation for the booking model
+        /// </summary>
+        /// <returns></returns>
+        public string ToCSV()
+            => $"{UserId},{FlightId},{(int)FlightClass}";
+
+        public bool EqualsTo(BookingModel to)
+            => this.UserId == to.UserId &&
+               this.FlightId == to.FlightId &&
+               this.FlightClass == to.FlightClass;
+
+    };
+}
---
+++ Tests/ATB.DA.Test/BookingsTests/BookingModelTests.cs
@@ -0,0 +1,69 @@
+﻿using ATB.DA.Enums;
+using ATB.DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATB.DA.Test.BookingsTests
+{
+    [TestClass]
+    public class BookingModelTests
+    {
+        [TestMethod]
+        public void ToCSV_ConvertsBookingModelToCSVString_Successful()
+        {
+            // Arrange
+            BookingModel bookingModel = new BookingModel(12345, 67890, FlightClassEnum.Business);
+
+            // Act
+            string csvString = bookingModel.ToCSV();
+
+            // Assert
+            string expectedCSV = "12345,67890,1"; // The expected CSV representation based on the provided FlightClassEnum mapping
+            Assert.AreEqual(expectedCSV, csvString);
+        }
+
+        [TestMethod]
+        public void FromCSV_ConvertsCSVStringToBookingModel_Successful()
+        {
+            // Arrange
+            string csvString = "98765,54321,2"; // CSV representation of BookingModel with FlightClassEnum mapped to '2' (e.g., Economy)
+
+            // Act
+            BookingModel bookingModel = BookingModel.FromCSV(csvString);
+
+            // Assert
+            Assert.AreEqual((ulong)98765, bookingModel.UserId);
+            Assert.AreEqual((ulong)54321u, bookingModel.FlightId);
+            Assert.AreEqual(FlightClassEnum.FirstClass, bookingModel.FlightClass);
+        }
+
+        [TestMethod]
+        public void ToCSV_FromCSV_ReturnsOriginalBookingModel_Successful()
+        {
+            // Arrange
+            BookingModel originalBookingModel = new BookingModel(12345, 67890, FlightClassEnum.Business);
+
+            // Act
+            string csvString = originalBookingModel.ToCSV();
+            BookingModel recreatedBookingModel = BookingModel.FromCSV(csvString);
+
+            // Assert
+            Assert.AreEqual(originalBookingModel.UserId, recreatedBookingModel.UserId);
+            Assert.AreEqual(originalBookingModel.FlightId, recreatedBookingModel.FlightId);
+            Assert.AreEqual(originalBookingModel.FlightClass, recreatedBookingModel.FlightClass);
+        }
+
+        [TestMethod]
+        public void FromCSV_InvalidCSVFormat_ThrowsException()
+        {
+            // Arrange
+            string invalidCsvString = "12345,67890"; // CSV format missing FlightClassEnum value
+
+            // Act & Assert
+            Assert.ThrowsException<IndexOutOfRangeException>(() => BookingModel.FromCSV(invalidCsvString));
+        }
+    }
+}

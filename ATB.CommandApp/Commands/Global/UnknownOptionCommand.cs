a0b3855e7bfa9541ccb53a4d2c5046e37f3b6a76
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-08-05 22:21:03 +0300

    ✅ Global Commands.
    - Global commands can be used in different commanders.

---
+++ ATB.CommandApp/Commands/Global/UnknownOptionCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATB.CommandApp.Commands.Global
+{
+    public class UnknownOptionCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Unkown option please try again.");
+        }
+    }
+}

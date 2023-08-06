080bcb508cf72dfdd65ea3c899c6988eb91cd8db
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-08-07 02:45:27 +0300

    ⚒️ Change the dependencies to static in the presentation layer.
    - To be able to use it across all the project without the need of have an object of the App

--- ATB.CommandApp/App.cs
+++ ATB.CommandApp/App.cs
@@ -1,9 +1,10 @@
-﻿using ATB.BL.Services.Authentication;
+using ATB.BL.Services.Authentication;
 using ATB.BL.Services.Booking;
 using ATB.BL.Services.Flight;
 using ATB.BL.Services.User;
 using ATB.CommandApp.Commands;
 using ATB.CommandApp.Commands.Entry;
+using ATB.DA.Enums;
 using ATB.DA.Repositories;
 using ATB.DA.Repositories.Bookings;
 using ATB.DA.Repositories.Flights;
@@ -13,10 +14,10 @@ namespace ATB.CommandApp
 {
     class App
     {   //Configurations :
-        public IAuthenticationServices Auth = new AuthenticationServices(new FileUserRepository());
-        public IUserServices UserServices = new UserServices(new FileUserRepository());
-        public IFlightServices FlightServices = new FlightServices(new FileFlightRepository());
-        public IBookingServices BookingServices = new BookingServices(new FileFlightRepository(), new FileBookingRepository());
+        public readonly static IAuthenticationServices Auth = new AuthenticationServices(new FileUserRepository());
+        public readonly static IUserServices UserServices = new UserServices(new FileUserRepository());
+        public readonly static IFlightServices FlightServices = new FlightServices(new FileFlightRepository());
+        public readonly static IBookingServices BookingServices = new BookingServices(new FileFlightRepository(), new FileBookingRepository());
         //End
 
         public void Launch()
@@ -28,7 +29,7 @@ namespace ATB.CommandApp
 
         public static void Main(string[] args)
         {
-           new App().Launch();
+            new App().Launch();
         }
     }
 }

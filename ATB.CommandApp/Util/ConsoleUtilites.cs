c3f15bd5ca0f07b2cc7abcb9170b37042cb40f53
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-08-07 02:54:58 +0300

    ➕ Add a console common ultilites.
    - for retrieving input from the user and validate it.

--- ATB.CommandApp/Util/ConsoleUtilites.cs
+++ ATB.CommandApp/Util/ConsoleUtilites.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ATB.DA.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -37,6 +39,89 @@ namespace ATB.CommandApp.Util
             }
 
             return email;
+        }
+        public static int? GetNullOrValidPrice()
+        {
+            string? price = Console.ReadLine();
+            
+            while (price != null && price != "" && !int.TryParse(price, out int result)) 
+            {
+                Console.WriteLine("Invalid input.");
+                price = Console.ReadLine();
+            }
+                
+
+            if (price == null || price == "")
+                return null;
+
+            return int.Parse(price);
+        }
+
+        public static string? GetNullOrNotEmptyString()
+        {
+            string? str = Console.ReadLine();
+
+            if (str is null || string.IsNullOrEmpty(str.Trim()))
+                return null;
+
+            return str.Trim();
+        }
+
+        public static DateTime? GetNullOrValidDate()
+        {
+            string? date = Console.ReadLine();
+
+            while (date is not null && date != "" && !DateTime.TryParse(date.Trim(), out DateTime result))
+            {
+                Console.WriteLine("Invalid Input.");
+                date = Console.ReadLine();
+            }
+
+            if(date is null || date.Trim() == string.Empty)
+                return null;
+
+            DateTime.TryParse(date.Trim(), out DateTime res);
+            return res;
+        }
+
+        internal static ulong? GetNullOrValidULong()
+        {
+            string? id = Console.ReadLine();
+
+            while (id != null && id != "" && !ulong.TryParse(id, out ulong result))
+            {
+                Console.WriteLine("Invalid input.");
+                id = Console.ReadLine();
+            }
+
+
+            if (id == null || id == "")
+                return null;
+
+            return ulong.Parse(id);
+        }
+
+        public static FlightClassEnum? GetNullOrFlightClass()
+        {
+            
+            var validClasses = new Dictionary<string, FlightClassEnum>();
+            validClasses.Add("economy", FlightClassEnum.Economy);
+            validClasses.Add("business", FlightClassEnum.Business);
+            validClasses.Add("first class", FlightClassEnum.FirstClass);
+
+            string? flightClass = Console.ReadLine();
+
+            while(flightClass != null && flightClass != "" && !validClasses.ContainsKey(flightClass.ToLower().Trim()))
+            {
+                Console.WriteLine("Invalid input.");
+                flightClass = Console.ReadLine();
+            }
+
+            if (flightClass is null || string.IsNullOrEmpty(flightClass.Trim()))
+                return null;
+
+            return validClasses[flightClass.ToLower().Trim()];
+
         }
     }
 }
